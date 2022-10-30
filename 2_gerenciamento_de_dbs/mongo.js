76e5097d1039a9fb3ecf42d6174636c9bfbdcfc1
Felipe Marques <felipe.marqs@outlook.com>
2022-10-30 09:47:22 -0300

    db

---
+++ 2_gerenciamento_de_dbs/mongo.js
@@ -0,0 +1,1 @@
+ db.carros.insertMany([{_id: "1000" , marca:"FIAT", modelo: "STRADA", ano: 2021} , {_id: "1001" , marca:" VOLKSWAGEM", modelo: "Gol", ano: 2012} , {_id: "1002" , marca:"CHEVROLET", modelo: "Prisma", ano: 2023}])
